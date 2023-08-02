3482f5037dbe6ad5edb42b7a425d5f46d7893422
Cora Johnson <cora.johnson@gmail.com>
2023-08-02 19:03:54 -0400

    commit v2.0 added Step 4 under Odin lesson.

--- Etch.js
+++ Etch.js
@@ -1,7 +1,13 @@
+//Math
+const L = 640;
+const W = 640;
+const Area = L * W;
+let row = 16;
+let length_box = L/row;
+let numb_box = Area/(length_box*length_box);
 
 
-
-// container 1
+// Create container 1 using DOM.
 const div_container = document.createElement("div");
 document.body.appendChild(div_container);
 div_container.classList.add("container");
@@ -10,44 +16,93 @@ div_container.classList.add("container");
 //css styles
 div_container.style.display = "inline-flex";
 div_container.style.flexWrap = "wrap";
-div_container.style.width = "690px";
-div_container.style.height = "640x";
+
+div_container.style.width = "640px";
+div_container.style.height = "640px";
+
 div_container.style.margin = "200px";
 div_container.style.marginTop = "0px";
 div_container.style.marginLeft = "600px";
 
 
-//children of container 1
+// create children of container 1 using DOM.
+
 function childDivone() {
+
 const div_one = document.createElement("div");
 div_one.classList.add("box");
 div_container.appendChild(div_one);
+ 
 
-
-// css styles
+// css styles for child containers Divs.
 div_one.style.backgroundColor = "white";
-div_one.style.width = "38px";
-div_one.style.height = "38px";
-div_one.style.border= "solid black";
+div_one.style.width = `${length_box}px`;
+div_one.style.height = `${length_box}px`;
+div_one.style.border = "solid black";
+div_one.style.boxSizing = "border-box";
 
 };
 
-// ======================================
+// Use For Loop to create the child container Divs.
 
-//loop to create children boxes
-
-for (i=1; i<=224; i++) {
+for (i=1; i<=numb_box; i++) {
 
     childDivone();
     
     };
 
-// EventListener
 
-let box = document.querySelectorAll(".box");
+// EventListener to create the hover effect.
+        
+ let box = document.querySelectorAll(".box");
+        
+ box.forEach(box => box.addEventListener('mouseover', () => {
+     box.style.backgroundColor = "pink";
+ }));
 
-box.forEach(box => box.addEventListener('mouseover', () => {
-    box.style.backgroundColor = "pink";
-}));
+ // create button
 
-// button
+const btn = document.createElement("button");
+document.body.appendChild(btn);
+btn.textContent = "Change Grid";
+btn.style.height = "70px";
+btn.style.width = "200px";
+
+//EventListener 2 
+
+btn.addEventListener('click', () => {
+    let result = prompt("Change the Grid Dimension. Enter a number between 1-100");
+    
+// Everything down below with the exception of "EventListener 1" is from CHATGPT everything else I constructed without help.
+
+    // Validate the input to ensure it's a number between 1 and 100
+    if (!isNaN(result) && result >= 1 && result <= 100) {
+        row = Number(result); // Convert the input to a number and update the 'row' variable
+        length_box = L / row; // Recalculate 'length_box' based on the new 'row'
+        numb_box = Area / (length_box * length_box); // Recalculate 'numb_box' based on the new 'length_box'
+        
+        // Remove the previous boxes
+        const existingBoxes = document.querySelectorAll(".box");
+        existingBoxes.forEach(box => box.remove());
+        
+        // Create the new boxes with the updated dimensions.
+        for (i = 1; i <= numb_box; i++) {
+            childDivone();
+        }
+    } else {
+        alert("Invalid input. Please enter a number between 1-100.");
+    }
+
+    // Add EventListener 1 again for the new boxes with updated dimensions.
+        
+    let box = document.querySelectorAll(".box");
+        
+    box.forEach(box => box.addEventListener('mouseover', () => {
+     box.style.backgroundColor = "pink";
+ }));
+});
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Etch-a-Sketch-Odin
 Etch-a-Sketch Project
+
+I learned a few things from this lesson as follows:
+
+#1 A user's input to a prompt returns as a string. To convert it to a number you can use the Nubmer() function
+
+#2 NaN stands for Not a Number and is a global property representation. You can use the isNan() function to determine if a variable is Not a Number. If it is not, the output will return true, otherwise false. 
+
+The !isNaN(result): By adding the "!" before isNaN(result), the result is inverted. So, if isNaN(result) returns true, !isNaN(result) will be false, and vice versa.
+
+#3 When you need to update global variables based on user input without resorting to "shadowing," you can directly modify the global variables within the function without declaring them with let, const, or var. By accessing the global variables without redeclaration, you can update their values from within the local scope 
+without creating new local variables.
+
+ In JavaScript, if you declare a variable with the same name inside a local scope (e.g., inside a function or block), it creates a new local variable that "shadows" the global variable with the same name. The local variable takes precedence over the global variable within that scope.
+However, modifying the local variable does not affect the value of the global variable. They are separate entities with different values.
+
+#4 The remove() method removes the element from the DOM. 
+
